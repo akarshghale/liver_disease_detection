92efe87993b006063be130251c91039b44e9e257
Akarsh Ghale <7480878+akarshghale@users.noreply.github.com>
2022-06-13 21:01:05 +0530

    Update liver_engine.py

--- liver_engine.py
+++ liver_engine.py
@@ -1,7 +1,6 @@
 # Commented out IPython magic to ensure Python compatibility.
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 
