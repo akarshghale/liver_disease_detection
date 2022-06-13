dc48b2e492af7c28afeb9a5565beec60310423b9
Akarsh Ghale <7480878+akarshghale@users.noreply.github.com>
2022-06-14 00:16:48 +0530

    Update liver_engine.py

--- liver_engine.py
+++ liver_engine.py
@@ -62,7 +62,9 @@ with st.sidebar:
 
 input_features_list = [add_age, add_Total_Bilibubin, add_Alkaline_Photophase, Alamine_Aminotransferase, Asparate_Aminotransferase]
 input_features = np.array(input_features_list)
+print(input_features)
 input_features = input_features.reshape(1, -1)
+print(input_features)
 
 if clicked:
     prediction = begin_scan(input_features)
